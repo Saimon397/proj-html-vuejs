075f082b4ace44161e58a78088c3b1a339992f2a
Saimon397 <simonecarrabba@gmail.com>
2022-11-22 21:49:00 +0100

    comm7.1

--- src/components/Main5Component.vue
+++ src/components/Main5Component.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="bgg container pt-5 pb-5 d-flex justify-content-center">
-        <div class="d-flex flex-column gap-3 ms-3">
+    <div class="bgg container-fluid pt-5 pb-5">
+        <div class="d-flex flex-column row gap-3">
             <div class="card2" v-for="(element, index) in elements3" :key="index">
                 <p>{{ element.cap }}</p>
                 <h5> {{ element.name }} </h5>
-                <div class="d-flex gap-3 justify-content-end">
-                    <span> {{ element.num }} </span>
-                    <span> {{ element.data }} </span>
-                    <span> {{ element.buttonn }} </span>
+                <div class="">
+                    <span class="num"> {{ element.num }} </span>
+                    <span class="data"> {{ element.data }} </span>
+                    <span class="button"> {{ element.buttonn }} </span>
                 </div>
             </div>
         </div>
@@ -79,16 +79,46 @@ export default {
     background-color: $col11;
     box-shadow: 10px 5px 5px $col5;
     width: 500px;
+    height: 200px;
+
+    p {
+        color: $col5;
+    }
+
+    h5 {
+        color: $col1-2;
+    }
 }
 
-div h2 {
-    color: #20ad96;
-    font-weight: bolder;
-    font-size: 4rem;
+.num {
+    color: $col4;
+    font-weight: bold;
+    font-size: 40px;
 }
 
-span {
-    font-weight: bolder;
-    color: black;
+.data {
+    color: $col1-2;
+    font-weight: 500;
+    font-size: 30px;
+}
+
+.button {
+    margin-top: 20px;
+    justify-content: center;
+    font-weight: bold;
+    padding: 18px 18px;
+    background-color: $col4;
+    border-radius: 4%;
+    width: 25%;
+    transition: .2s;
+    color: $col1;
+
+    &:hover {
+        background-color: $col1-1;
+    }
+
+    a {
+        text-decoration: none;
+    }
 }
 </style>
